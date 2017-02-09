dc30f085bf748284815c6a2911141098ee848a9b
A T M <527526@CTS.COM>
2017-02-09 19:13:21 +0530

    addition after altering some sections

--- iamx/sendemail.php
+++ iamx/sendemail.php
@@ -6,7 +6,7 @@
     header( 'Content-type: application/json' );
 
 
-    $to         = 'trendytheme.net@gmail.com';  // put your email here
+    $to         = 'atmvijay1993@gmail.com';  // put your email here
 
     $email_template = 'simple.html';
 
